9c32bce7e7659edf1573c6fa63d398c2d64b6881
Bruna Domingues Leite <brunadomingues.dev@gmail.com>
2022-11-30 15:08:13 -0300

    first commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,40 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+export function Comment({content, onDeleteComment}: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+    function handleLikeComment(){
+        setLikeCount((state)=>{
+            return state + 1;
+        });
+        //atualizar info dependendo da informação anterior fazer assim
+    }
+    return(
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/BrunaDomingues.png/"/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Bruna Domingues</strong>
+                            <time title='11 de maio às 08:13' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'><Trash size={24}/></button>
+                    </header>  
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}><ThumbsUp/> Aplaudir <span>{likeCount}</span></button>
+                </footer>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Post.tsx
@@ -0,0 +1,90 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR  from 'date-fns/locale/pt-BR'
+import { Comment } from './Comment'; 
+import { Avatar } from './Avatar';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+import styles from './Post.module.css';
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+}
+interface PostProps {
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+export function Post({author, publishedAt, content}: PostProps){
+    const [comments, setComments] = useState(['Muito bom Devon, parabéns!! 👏👏']);
+    const publishedDateFormated = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBR});
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+    const [newCommentText, setNewCommentText] = useState('');
+    function handleCreateNewComment(event: FormEvent){
+        event.preventDefault();
+        setComments([...comments,newCommentText]);
+        setNewCommentText('');
+    }
+    function handleNewCommentChange(event:ChangeEvent<HTMLTextAreaElement>){
+        event.target.setCustomValidity('');
+        setNewCommentText(event.target.value);
+    }
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>){
+        event.target.setCustomValidity('Esse campo é obrigatório!');
+    }
+    function deleteComment(commentToDelete: string){
+        const commentsWithoutDeleteOne = comments.filter(comment => {
+            return comment != commentToDelete;
+        })
+        setComments(commentsWithoutDeleteOne);
+    }
+    const isNewCommentEmpty = newCommentText.length==0;
+    return(
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <Avatar src={author.avatarUrl} />
+                    <div className={styles.authorInfo}>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
+                    </div>
+                </div>
+                <time title={publishedDateFormated} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+            </header>
+            <div className={styles.content}>
+                {content.map(line => {
+                    if(line.type=='paragraph'){
+                        return <p key={line.content}>{line.content}</p>
+                    }  else if(line.type=='link'){
+                        return <p key={line.content}><a href='#'>{line.content}</a></p>
+                    }
+                })}
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea name="comment" placeholder='Deixe um comentário' onChange={handleNewCommentChange} value={newCommentText} onInvalid={handleNewCommentInvalid} required/>
+                <footer>
+                    <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
+                </footer>
+            </form>
+            <div className={styles.commentList}>
+                {comments.map(
+                    comment => {
+                        return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>;
+                    }
+                )}
+            </div>
+        </article>
+    )
+}
